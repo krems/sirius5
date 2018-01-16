bf576ce8eb3dfe33e6f3470695c99e4135058e33
Valerii Ovchinnikov <valerii.ovchinnikov@revolut.com>
2018-01-16 10:26:21 +0300

    ;

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -9,14 +9,17 @@ create table client (
     name varchar(30),
     rating float,
     primary key (id));
+
 create table license (
     id int,
     primary key (id));
+
 create table driver (
     id int,
     license int,
     primary key (id),
     foreign key license(id));
+
 create table ride (
     id int,
     driver int,
@@ -28,7 +31,7 @@ create table ride (
     foreign key (client) references client(id),
     foreign key (car) references cars(id));
 
-insert into cars values (1, 'white', 'A000AA000')
+insert into cars values (1, 'white', 'A000AA000');
 insert into client values (1, 'Valerii', 4.8);
 insert into license values (1);
 insert into driver values (1, 1);
