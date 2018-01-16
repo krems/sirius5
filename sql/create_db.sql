8dbfc0ccda2f2f3069e14540364670fe9b3c82d9
Valerii Ovchinnikov <valerii.ovchinnikov@revolut.com>
2018-01-16 20:16:19 +0300

    fk syntax;

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -16,7 +16,7 @@ create table driver (
     id int,
     license int,
     primary key (id),
-    foreign key license(id));
+    foreign key (license) references license(id));
 create table ride (
     id int,
     driver int,
@@ -28,7 +28,7 @@ create table ride (
     foreign key (client) references client(id),
     foreign key (car) references cars(id));
 
-insert into cars values (1, 'white', 'A000AA000')
+insert into cars values (1, 'white', 'A000AA000');
 insert into client values (1, 'Valerii', 4.8);
 insert into license values (1);
 insert into driver values (1, 1);
